5dfef68b000e25b3a3faf2fcf6b52729c2a08104
Hongyiel Suh <suhho@oregonsate.edu>
2021-01-26 16:21:50 +0900

    get access twitch

---
+++ src/main.py
@@ -0,0 +1,36 @@
+# checks whether a twitch.tv userstream is live
+import requests
+
+URL = 'https://api.twitch.tv/helix/streams?user_login=woowakgood'
+authURL = 'https://id.twitch.tv/oauth2/token'
+
+# This could be change
+Client_ID = 'type your client ID'
+Secret  = 'type your secret here'
+
+getInfo = {'client_id': Client_ID,
+             'client_secret': Secret,
+             'grant_type': 'client_credentials'
+             }
+
+def Check():
+    callInfo = requests.post(url=authURL, params=getInfo)
+    access_token = callInfo.json()['access_token']
+
+    head = {
+    'Client-ID' : Client_ID,
+    'Authorization' :  "Bearer " + access_token
+    }
+
+    checkRequest = requests.get(URL, headers = head).json()['data']
+
+    if checkRequest:
+        checkRequest = checkRequest[0]
+        if checkRequest['type'] == 'live':
+            return True
+        else:
+            return False
+    else:
+        return False
+
+print(Check())
